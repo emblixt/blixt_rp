34bf6eb27da9807cc14ee01e144ef0338d2da80b
MårtenBlixt <marten.blixt@gmail.com>
2020-04-16 12:50:34 +0200

    Creating a FluidMix object

--- core/fluids.py
+++ core/fluids.py
@@ -204,8 +204,81 @@ class Fluid(object):
             logger.warning(warn_txt)
         return object.__getattribute__(self, 'rho')
 
-class FluidSet(object):
-    
+
+class FluidSet_RETIRE(object):
+
+    def __init__(self,
+                 name=None,
+                 fluids=None,
+                 header=None):
+        """
+        :param fluids:
+            dict
+            Dictionary of Param objects
+        """
+        self.header = Header(header)
+        if fluids is None:
+            fluids = {}
+        self.fluids = fluids
+
+        if name is None:
+            name = 'MyFluids'
+        self.name = name
+
+    def __str__(self):
+        out = ''
+        for key in ['initial', 'final']:
+            out += "{} fluids:\n".format(key)
+            for f in list(self.fluids[key].keys()):
+                out += "  {}\n".format(f)
+                out += "    K: {}, Mu: {}, Rho {}\n".format(self.fluids[key][f].k.value,
+                                                            self.fluids[key][f].mu.value,
+                                                            self.fluids[key][f].rho.value)
+                out += "    Calc. method: {}\n".format(self.fluids[key][f].calculation_method.value)
+                out += "    Volume fraction: {}\n".format(self.fluids[key][f].volume_fraction)
+        return out
+
+    def read_excel(self, filename, sheet_name='Fluids', header=1):
+        fluids = {
+            'initial': {},
+            'final': {}
+        }
+        table = pd.read_excel(filename, sheet_name=sheet_name, header=header)
+        for i, name in enumerate(table['Name']):
+            vf = table['Volume fraction'][i]
+            if isnan(vf):
+                continue  # Avoid fluids where the volume fraction is not set
+            if table['Calculation method'][i] == 'Batzle and Wang':
+                warn_txt = 'Calculation of fluid properties is still not implemented, please use constant values'
+                print('WARNING {}'.format(warn_txt))
+                logger.warning(warn_txt)
+            this_fluid = Fluid(
+                table['Calculation method'][i],
+                float(table['Bulk moduli [GPa]'][i]),
+                float(table['Shear moduli [GPa]'][i]),
+                float(table['Density [g/cm3]'][i]),
+                float(table['T gradient [deg C/m]'][i]),
+                float(table['T ref [C]'][i]),
+                float(table['P gradient [MPa/m]'][i]),
+                float(table['P ref [MPa]'][i]),
+                float(table['Salinity [ppm]'][i]),
+                float(table['GOR'][i]),
+                float(table['Oil API'][i]),
+                float(table['Gas gravity'][i]),
+                table['Gas mixing'][i],
+                float(table['Brie exponent'][i]),
+                name=name.lower(),
+                volume_fraction=float(vf) if (not isinstance(vf, str)) else vf.lower()
+            )
+            fluids[table['Substitution order'][i].lower()][name.lower()] = this_fluid
+        self.fluids = fluids
+        self.header['orig_file'] = filename
+
+
+class FluidMix(object):
+    """
+    Dictionary containing the initial, and final, fluids for given wells and intervals
+    """
     def __init__(self,
                  name=None,
                  fluids=None, 
@@ -238,41 +311,79 @@ class FluidSet(object):
         return out
 
 
-    def read_excel(self, filename, sheet_name='Fluids', header=1):
-        fluids = {
-            'initial': {},
-            'final': {}
-        }
-        table = pd.read_excel(filename, sheet_name=sheet_name, header=header)
-        for i, name in enumerate(table['Name']):
-            vf = table['Volume fraction'][i]
-            if isnan(vf):
-                continue  # Avoid fluids where the volume fraction is not set
-            if table['Calculation method'][i] == 'Batzle and Wang':
+    def read_excel(self, filename,
+                   fluid_sheet='Fluids', fluid_header=1,
+                   mix_sheet='Fluid mixtures', mix_header=1):
+
+        # First read in all fluids defined in the project table
+        all_fluids = {}
+        fluids_table = pd.read_excel(filename,
+                                     sheet_name=fluid_sheet, header=fluid_header)
+
+        for i, name in enumerate(fluids_table['Name']):
+            if isnan(name):
+                continue  # Avoid empty lines
+            if fluids_table['Calculation method'][i] == 'Batzle and Wang':
                 warn_txt = 'Calculation of fluid properties is still not implemented, please use constant values'
                 print('WARNING {}'.format(warn_txt))
                 logger.warning(warn_txt)
             this_fluid = Fluid(
-                    table['Calculation method'][i],
-                    float(table['Bulk moduli [GPa]'][i]),
-                    float(table['Shear moduli [GPa]'][i]),
-                    float(table['Density [g/cm3]'][i]),
-                    float(table['T gradient [deg C/m]'][i]),
-                    float(table['T ref [C]'][i]),
-                    float(table['P gradient [MPa/m]'][i]),
-                    float(table['P ref [MPa]'][i]), 
-                    float(table['Salinity [ppm]'][i]), 
-                    float(table['GOR'][i]),
-                    float(table['Oil API'][i]), 
-                    float(table['Gas gravity'][i]),
-                    table['Gas mixing'][i],
-                    float(table['Brie exponent'][i]),
-                    name=name.lower(),
-                    volume_fraction=float(vf) if (not isinstance(vf, str)) else vf.lower()
+                    fluids_table['Calculation method'][i],
+                    float(fluids_table['Bulk moduli [GPa]'][i]),
+                    float(fluids_table['Shear moduli [GPa]'][i]),
+                    float(fluids_table['Density [g/cm3]'][i]),
+                    float(fluids_table['T gradient [deg C/m]'][i]),
+                    float(fluids_table['T ref [C]'][i]),
+                    float(fluids_table['P gradient [MPa/m]'][i]),
+                    float(fluids_table['P ref [MPa]'][i]),
+                    float(fluids_table['Salinity [ppm]'][i]),
+                    float(fluids_table['GOR'][i]),
+                    float(fluids_table['Oil API'][i]),
+                    float(fluids_table['Gas gravity'][i]),
+                    fluids_table['Gas mixing'][i],
+                    float(fluids_table['Brie exponent'][i]),
+                    name=name.lower()
             )
-            fluids[table['Substitution order'][i].lower()][name.lower()] = this_fluid
-        self.fluids = fluids
-        self.header['orig_file'] = filename
+            all_fluids[name.lower()] = this_fluid
+
+        # Then read in the fluid mixes
+        fluids_mixes = {
+            'initial': {},
+            'final': {}
+        }
+        mix_table = pd.read_excel(filename, sheet_name=mix_sheet, header=mix_header)
+        for i, name in enumerate(mix_table['Fluid name']):
+            vf = mix_table['Volume fraction'][i]
+            if isnan(vf):
+                continue  # Avoid fluids where the volume fraction is not set
+            this_subst = mix_table['Substitution order'].lower()
+            this_well = mix_table['Well name'].upper()
+            this_wi = mix_table['Interval name'].upper()
+            this_fluid = all_fluids[name.lower()]
+            this_fluid.volume_fraction = \
+                float(vf) if (not isinstance(vf, str)) else vf.lower()
+            # iterate down in this complex dictionary
+            # {this_subst:                          # 1'st level
+            #       {this_well:                     # 2'nd level
+            #           {this_wi:                   # 3'd level
+            #               {fluid_name: Fluid()    # 4'th level
+            #       }}}}
+            # 2'nd level
+            if this_well in list(fluids_mixes[this_subst].keys()):
+                # 3'rd level
+                if not isinstance(fluids_mixes[this_subst][this_well], dict):
+                    fluids_mixes[this_subst][this_well] = {}
+                if this_wi in list(fluids_mixes[this_subst][this_well].keys()):
+                    # 4'th level
+                    if not isinstance(fluids_mixes[this_subst][this_well][this_wi], dict):
+                        fluids_mixes[this_subst][this_well][this_wi] = {}
+                    fluids_mixes[this_subst][this_well][this_wi][this_fluid.name.lower()] \
+                        = this_fluid
+
+
+        #self.fluids = fluids
+        #self.header['orig_file'] = filename
+        return all_fluids
 
 
 def test_fluidsub():
